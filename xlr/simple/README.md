87fb4203f167d433b92088f0211cae712e13c83f
Erik Rasmussen <erasmussen@xebialabs.com>
2017-09-13 19:07:47 -0600

    Updated READMEs

--- xlr/simple/README.md
+++ xlr/simple/README.md
@@ -1,4 +1,13 @@
 # XL Release Simple Plugin
 
 ## Problem Statement
-Simple problem statement...
+Acme Corporation has a simple Build and Test template ([Template](templates/XL_Release_Simple_Plugin.groovy)):
+
+![Template](images/template.png)
+
+As you can see in the template, a user manually finds a template for the sub-release and then has to input that into the "Start Build Sub-Release" task.
+
+In this exercise, create a plugin with a custom automated task that replaces the "Find Release Template" manual task, and does the following:
+- Takes the name of a template to search for.
+- Locates the XL Release Template with the specified name.
+- Outputs the ID of that Template so it can be stored in a variable and used by the "Start Build Sub-Release" task.
--- xlr/simple/templates/XL_Release_Simple_Plugin.groovy
+++ xlr/simple/templates/XL_Release_Simple_Plugin.groovy
@@ -1,19 +1,54 @@
-// Exported from:        http://shadow.hunyuk.org:5516/#/templates/Folder840512222-Release590441828/xfile
-// XL Release version:   7.1.0
-// Date created:         Tue Sep 12 10:40:59 MDT 2017
-
+def server(type, title) {
+    def cis = configurationApi.searchByTypeAndTitle(type, title)
+    if (cis.isEmpty()) {
+        throw new RuntimeException("No CI found for the type '${type}' and title '${title}'")
+    }
+    if (cis.size() > 1) {
+        throw new RuntimeException("More than one CI found for the type '${type}' and title '${title}'")
+    }
+    cis.get(0)
+}
+def server1 = server('xldeploy.Server','XL Deploy')
 xlr {
   release('XL_Release_Simple_Plugin') {
+    variables {
+      stringVariable('BUILD_RELEASE_ID') {
+        required false
+        showOnReleaseStart false
+      }
+    }
     scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2017-09-12T09:00:00-0600')
     phases {
-      phase('Simple Phase') {
+      phase('Build') {
         color '#009CDB'
         tasks {
-          custom('Simple Example Task') {
-            script {
-              type 'simple.ExampleTask'
-              example_property 'FOO'
+          manual('Find Release Template') {
+            owner 'admin'
+          }
+          createRelease('Start Build Sub-Release') {
+            createdReleaseId '${BUILD_RELEASE_ID}'
+          }
+          gate('Wait For Build') {
+            description 'Wait for build sub-release to complete.'
+            dependencies {
+              dependency {
+                variable 'BUILD_RELEASE_ID'
+              }
             }
+          }
+        }
+      }
+      phase('Test') {
+        color '#009CDB'
+        tasks {
+          custom('Deploy to Test') {
+            script {
+              type 'xldeploy.Deploy'
+              server server1
+            }
+          }
+          manual('Test Code') {
+            owner 'admin'
           }
         }
       }
